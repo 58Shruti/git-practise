70d624f2182fc3dfd62e41846f14a7fb56ac02f2
dev <dev@examplegmail.com>
2025-04-29 18:41:41 +0530

    comented lorem in testing file

--- testing.js
+++ testing.js
@@ -10,3 +10,8 @@ console.log('hiii')
 
 console.log('pick me -------------lkjlkfvb')
 console.log(square(9))
+
+
+
+// What is Lorem Ipsum?
+// Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
